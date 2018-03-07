11f3fc76e312d1ba0e724a56543a0fb7d3052427
Amber Elferink <a.s.elferink@students.uu.nl>
2018-03-07 15:22:45 +0100

    redundant codes weggegooid

--- javascript/plot.js
+++ javascript/plot.js
@@ -1,93 +1,88 @@
-var data = [[1, 130], [2, 40], [3, 80], [4, 160], [5, 159], [6, 370], [7, 330], [8, 350], [9, 370], [10, 400], [11, 330], [12, 350]];
+$(function() {
 
-var watermelondata = [
-    //g per 100g
-    [0, 30], //Calories
-    [1, 0], //Total Fat
-    [2, 0], //Cholesterol
-    [3, 1], //Sodium
-    [4, 0.112], //Potassium
-    [5, 8],  //Carbohydrates
-    [6, 0], //Dietary Fiber
-    [7, 6], //Sugars
-    [8, 1] //Protein
-];
-var fetadata = [
-    [0, 264], //Calories
-    [1, 21], //Total Fat
-    [2, 0.89], //Cholesterol
-    [3, 0.917], //Sodium
-    [4, 0.62], //Potassium
-    [5, 4],  //Carbohydrates
-    [6, 0], //Dietary Fiber
-    [7, 4], //Sugars
-    [8, 14] //Protein
-];
-var ticks = [
-    [0, "Calories"], //Calories
-    [1, "Total Fat"], //Total Fat
-    [2, "Cholesterol"], //Cholesterol
-    [3, "Sodium"], //Sodium
-    [4, "Potassium"], //Potassium
-    [5, "Carbohydrates"],  //Carbohydrates
-    [6, "Dietary Fiber"], //Dietary Fiber
-    [7, "Sugars"], //Sugars
-    [8, "Proteins"] //Protein
-];
+    var watermelondata = [[0, 30], //Calories
+        [1, 0], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 1], //Sodium
+        [4, 0.112], //Potassium
+        [5, 8],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 6], //Sugars
+        [8, 1] //Protein
+    ];
 
+    var fetadata = [    [0, 264], //Calories
+        [1, 21], //Total Fat
+        [2, 0.89], //Cholesterol
+        [3, 0.917], //Sodium
+        [4, 0.62], //Potassium
+        [5, 4],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 4], //Sugars
+        [8, 14] //Protein
+    ];
 
-//LET OP: geeft rare zooi omdat 1-8 dubbel is.
-var alldata = [watermelondata, fetadata];
+    var ticks = [
+        [0, "Calories"], //Calories
+        [1, "Total Fat"], //Total Fat
+        [2, "Cholesterol"], //Cholesterol
+        [3, "Sodium"], //Sodium
+        [4, "Potassium"], //Potassium
+        [5, "Carbohydrates"],  //Carbohydrates
+        [6, "Dietary Fiber"], //Dietary Fiber
+        [7, "Sugars"], //Sugars
+        [8, "Proteins"] //Protein
+    ];
 
-var dataset = [{label: "watermelon g/100g",data: alldata }];
+    var alldata = [watermelondata, fetadata];
 
-var stack = 0,
-    bars = true,
-    lines = false,
-    steps = false;
-
-var options = {
-    series: {
-        stack: stack,
-        lines: {
-            show: lines,
-            fill: true,
-            steps: steps
+    var options1 = {
+        series: {
+            stack: 0,
+            lines: {
+                show: false,
+                fill: true,
+                steps: false
+            },
+            bars: {show: true}
         },
-        bars: {show: true}
-    },
         bars: {
             align: "center",
             barWidth: 0.5
         },
-    xaxis: {
-        axisLabel: "Nutrition",
-        axisLabelUseCanvas: true,
-        ticks: ticks,
-        axisLabelPadding: 10
-    },
-    yaxis: {
-        axisLabel: "g/ 100g",
-        max: 100,
-        min: 0
+        xaxis: {
+            axisLabel: "Nutrition",
+            axisLabelUseCanvas: true,
+            ticks: ticks,
+            axisLabelPadding: 10
+        },
+        yaxis: {
+            axisLabel: "g/ 100g",
+            max: 100,
+            min: 0
+        }
+    };
+    function plotChart() {
+        $.plot(".plotIngredients", alldata, options1);
     }
-};
 
-$(document).ready(function () {
-    $.plot($("#plotIngredients"), dataset, options);
+    plotChart();
+
+    $(".stackControls button").click(function (e) {
+        e.preventDefault();
+        stack = $(this).text() == "With stacking" ? true : null;
+        plotChart();
+    });
+
+    $(".graphControls button").click(function (e) {
+        e.preventDefault();
+        bars = $(this).text().indexOf("Bars") != -1;
+        lines = $(this).text().indexOf("Lines") != -1;
+        steps = $(this).text().indexOf("steps") != -1;
+        plotChart();
+    });
+
+    // Add the Flot version string to the footer
+
+    $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
 });
-
-
-
-
-/*function makePercentage(dataproces) {
-
-for(var i = 0; i < dataproces.length; i++)
-{
-    for(var j=0; j<dataproces[i].length; j++)
-    {
-        var datacopy = dataproces[i][j/100];
-    }
-}
-return dataproces;
-}*/
