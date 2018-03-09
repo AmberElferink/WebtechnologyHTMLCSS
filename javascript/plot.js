3d2bc906b6d374a47562d419cd8d74263f5ed599
anneloesmeijer <a.l.meijer2@students.uu.nl>
2018-03-09 11:35:06 +0100

    alle ingredienten voor de plot ingevoerd

--- javascript/plot.js
+++ javascript/plot.js
@@ -1,6 +1,8 @@
 $(function() {
 
-    var watermelondata = [[0, 30], //Calories
+ //data for the watermelon and feta starter recipe
+    var watermelondata = [
+        [0, 30], //Calories
         [1, 0], //Total Fat
         [2, 0], //Cholesterol
         [3, 1], //Sodium
@@ -11,15 +13,174 @@ $(function() {
         [8, 1] //Protein
     ];
 
-    var fetadata = [    [0, 264], //Calories
+    var fetadata = [
+        [0, 264], //Calories
         [1, 21], //Total Fat
-        [2, 0.89], //Cholesterol
+        [2, 0.089], //Cholesterol
         [3, 0.917], //Sodium
         [4, 0.62], //Potassium
         [5, 4],  //Carbohydrates
         [6, 0], //Dietary Fiber
         [7, 4], //Sugars
         [8, 14] //Protein
+    ];
+
+    var redoniondata = [
+        [0, 37], //Calories
+        [1, 0], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0.002], //Sodium
+        [4, 0.185], //Potassium
+        [5, 7],  //Carbohydrates
+        [6, 2], //Dietary Fiber
+        [7, 5], //Sugars
+        [8, 1] //Protein
+    ];
+    var mintdata = [
+        [0, 0], //Calories
+        [1, 1], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0], //Sodium
+        [4, 0], //Potassium
+        [5, 8],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 0], //Sugars
+        [8, 3] //Protein
+    ];
+    var oliveoildata = [
+        [0, 883], //Calories
+        [1, 100], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 1], //Sodium
+        [4, 0], //Potassium
+        [5, 0],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 0], //Sugars
+        [8, 0] //Protein
+    ];
+
+// data for the pepper soup main course recipe
+
+    var whiteoniondata = [
+        [0, 175], //Calories
+        [1, 5], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0], //Sodium
+        [4, 0], //Potassium
+        [5, 0],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 0], //Sugars
+        [8, 0] //Protein
+    ];
+
+    var garlicdata = [
+        [0, 236], //Calories
+        [1, 13], //Total Fat
+        [2, 0.012], //Cholesterol
+        [3, 0.002], //Sodium
+        [4, 0.6], //Potassium
+        [5, 18],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 6], //Sugars
+        [8, 0] //Protein
+    ];
+
+    var redpepperdata = [
+        [0, 25], //Calories
+        [1, 0.1], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0], //Sodium
+        [4, 0], //Potassium
+        [5, 4.5],  //Carbohydrates
+        [6, 2], //Dietary Fiber
+        [7, 4.5], //Sugars
+        [8, 0.8] //Protein
+    ];
+
+    var yellowpepperdata = [
+        [0, 70], //Calories
+        [1, 5], //Total Fat
+        [2, 0.015], //Cholesterol
+        [3, 0.250], //Sodium
+        [4, 0], //Potassium
+        [5, 2],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 1], //Sugars
+        [8, 4] //Protein
+    ];
+
+    var sourcremedata = [
+        [0, 610], //Calories
+        [1, 19], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0.180], //Sodium
+        [4, 0], //Potassium
+        [5, 54],  //Carbohydrates
+        [6, 9], //Dietary Fiber
+        [7, 9], //Sugars
+        [8, 51] //Protein
+    ];
+
+    //data for the chocolate mouse dessert recipe
+
+    var chocolatedata = [
+        [0, 435], //Calories
+        [1, 14], //Total Fat
+        [2, 0.033], //Cholesterol
+        [3, 0.235], //Sodium
+        [4, 0.305], //Potassium
+        [5, 32],  //Carbohydrates
+        [6, 3], //Dietary Fiber
+        [7, 42], //Sugars
+        [8, 21] //Protein
+    ];
+
+    var cherriesdata = [
+        [0, 65], //Calories
+        [1, 0], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0], //Sodium
+        [4, 0], //Potassium
+        [5, 15],  //Carbohydrates
+        [6, 0.7], //Dietary Fiber
+        [7, 15], //Sugars
+        [8, 1] //Protein
+    ];
+
+    var douclecreamdata = [
+        [0, 290], //Calories
+        [1, 30], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 1], //Sodium
+        [4, 0], //Potassium
+        [5, 3.5],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 3.5], //Sugars
+        [8, 2.5] //Protein
+    ];
+
+    var eggsdata = [
+        [0, 145], //Calories
+        [1, 10], //Total Fat
+        [2, 0.057], //Cholesterol
+        [3, 1], //Sodium
+        [4, 0], //Potassium
+        [5, 8],  //Carbohydrates
+        [6, 1], //Dietary Fiber
+        [7, 1], //Sugars
+        [8, 24] //Protein
+    ];
+
+    var goldensugardata = [
+        [0, 396], //Calories
+        [1, 0], //Total Fat
+        [2, 0], //Cholesterol
+        [3, 0], //Sodium
+        [4, 0], //Potassium
+        [5, 99],  //Carbohydrates
+        [6, 0], //Dietary Fiber
+        [7, 99], //Sugars
+        [8, 0] //Protein
     ];
 
     var ticks = [
